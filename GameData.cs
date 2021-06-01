6ad8cb6fb4c9421239d6c2f6858893f358f20ea5
MeidosFriend <84859855+MeidosFriend@users.noreply.github.com>
2021-06-01 22:13:07 +0200

    Kiss->SCourt

--- GameData.cs
+++ GameData.cs
@@ -10,7 +10,7 @@ namespace DLC_Checker
         public static readonly string DLC_LIST_FILE = "COM_INM_NewListDLC.lst";
         public static readonly string MY_DLC_LIST_FILE = "MY_COM_INM_NewListDLC.lst";
         public static readonly string GAME_HEADER = "         " + GAME_NAME + "_DLC_Checker   |   Github.com/MeidosFriend/" + GAME_NAME + "_DLC_Checker";
-        public static readonly string GAME_REGISTRY = "HKEY_CURRENT_USER\\" + "SOFTWARE\\KISS\\CUSTOM ORDER MAID3D 2 It's a Night Magic";
+        public static readonly string GAME_REGISTRY = "HKEY_CURRENT_USER\\" + "SOFTWARE\\SCourt\\CUSTOM ORDER MAID3D 2 It's a Night Magic";
         
         public static string DLC_URL = "https://raw.githubusercontent.com/MeidosFriend/COM3D2_INM_DLC_Checker/master/COM_INM_NewListDLC.lst";
         public static string DLC_LIST_PATH = Path.Combine(Directory.GetCurrentDirectory(), DLC_LIST_FILE);
